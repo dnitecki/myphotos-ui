ed0fd3038de392b5c55afc3584ac97bebd4fd7c7
dnitecki <dominick.nitecki@outlook.com>
2023-12-21 00:36:01 -0500

    update

--- src/pages/photoGallery/PhotoGallery.tsx
+++ src/pages/photoGallery/PhotoGallery.tsx
@@ -1,18 +1,39 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
 import "./PhotoGallery.scss";
 import { getPhotosList } from "../../services/photoService";
 import { useQuery } from "react-query";
 
 export default function PhotoGallery() {
+  const [clicked, setClicked] = useState(false);
+  const [image, setImage] = useState(null);
+
   const { isLoading: isPhotosLoading, data: PhotoData } = useQuery(
     ["photoList"],
     getPhotosList
   );
-  const wrapperRef = useRef<HTMLDivElement>(null);
-  // const imagesLoaded = useOnLoadImages(wrapperRef);
 
-  return (
-    <div className="photo-gallery-container" ref={wrapperRef}>
+  const resetClick = () => {
+    setClicked(!clicked);
+  };
+
+  const imageClickHandler = (
+    farm: string,
+    server: string,
+    id: string,
+    secret: any
+  ) => {
+    const image: JSX.Element = (
+      <img
+        src={`https://farm${farm}.staticflickr.com/${server}/${id}_${secret}.png`}
+        alt={id}
+      />
+    );
+    setClicked(!clicked);
+    setImage(image);
+  };
+
+  const photoGallery = (
+    <>
       <header className="photo-gallery-header">
         <h1>Dom's Snaps</h1>
       </header>
@@ -23,6 +44,14 @@ export default function PhotoGallery() {
               <img
                 src={`https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.png`}
                 alt={photo.id}
+                onClick={() =>
+                  imageClickHandler(
+                    photo.farm,
+                    photo.server,
+                    photo.id,
+                    photo.secret
+                  )
+                }
               />
             </li>
           ))}
@@ -30,6 +59,23 @@ export default function PhotoGallery() {
       ) : (
         <div className="photo-gallery-background" />
       )}
-    </div>
+      ;
+    </>
+  );
+
+  const imageDetails = (
+    <>
+      <div className="photo-item-details" onClick={resetClick}>
+        {image}
+      </div>
+    </>
+  );
+
+  return (
+    <>
+      <div className="photo-gallery-container">
+        {clicked ? <>{imageDetails}</> : <>{photoGallery}</>}
+      </div>
+    </>
   );
 }
