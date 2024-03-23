53038d718e8f6bfe37f2ee3c1d9b817cf4c4d0e4
dnitecki <dominick.nitecki@outlook.com>
2024-03-23 14:41:33 -0400

    remove dot

--- src/utils/photoUtils.ts
+++ src/utils/photoUtils.ts
@@ -56,9 +56,7 @@ export const configureLocationList = (locationData: any) => {
       icon: LocationOnRoundedIcon,
       value: `${
         locationData?.locality?._content ?? locationData?.county?._content
-      }, ${locationData?.region?._content}, ${
-        locationData?.country?._content
-      }.`,
+      }, ${locationData?.region?._content}, ${locationData?.country?._content}`,
     },
   ];
   return { latLong: latLong, locationDataList: locationDataList };
